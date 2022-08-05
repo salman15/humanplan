85ef0990bc1fe927cd7b63d75666504e5c3c9c29
salman <salmannova@gmail.com>
2022-08-05 17:52:17 +0200

    updated title

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-npm install > install_output.log
-npm run build > build_output.log
+/opt/plesk/node/12/bin/npm install > install_output.log
+/opt/plesk/node/12/bin/npm run build > build_output.log
 touch deployment.txt 
 # touch output.log 
